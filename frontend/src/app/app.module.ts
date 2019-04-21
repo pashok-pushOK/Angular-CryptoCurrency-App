ed750851e2b5b651a4eb1dbd1fa16b317c58c5ad
Paul Tseluiko <pavel.tseluiko@gmail.com>
2019-04-21 13:36:11 +0300

    init some components, start develop project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,27 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 // routes
+import {AppRoutingModule} from './routes/app-routing/app-routing.module';
 
 import {AppComponent} from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CryptoItemComponent } from './crypto-item/crypto-item.component';
+import { CryptoItemPageComponent } from './crypto-item-page/crypto-item-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        CryptoItemComponent,
+        CryptoItemPageComponent,
+        NavbarComponent,
+        FooterComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
