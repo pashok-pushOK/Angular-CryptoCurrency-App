d14264a64f82f67046116966b17f23a07e3acf1a
Paul Tseluiko <pavel.tseluiko@gmail.com>
2019-04-30 15:29:44 +0300

    fixes

--- frontend/src/app/crypto-item.ts
+++ frontend/src/app/crypto-item.ts
@@ -2,5 +2,12 @@ export class CryptoItem {
     public id: number;
     public name: string;
     public symbol: string;
-    public quote: object;
+    // public quote: object;
+    public quote = {
+        USD: {
+            price: '',
+            percent_change_1h: '',
+            percent_change_24h: ''
+        }
+    }
 }
